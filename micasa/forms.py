08d7378a6cec9ed9442639f5cd496615eb2d690f
melisajuma <Melisaakinyi95@gmail.com>
2019-09-17 17:02:45 +0300

    add class

--- micasa/forms.py
+++ micasa/forms.py
@@ -3,3 +3,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import *
 
+class HoodForm(forms.ModelForm):
+    class Meta:
+        model = Hood
+        fields = ['name', 'location','image',]
+
+
