bd4e5404ed7419330274861336f8e4eb4a72fced
melisajuma <Melisaakinyi95@gmail.com>
2019-09-17 16:50:31 +0300

    add static details chnages

--- hood/settings.py
+++ hood/settings.py
@@ -151,14 +151,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = 
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
 ]
 
-LOGIN_REDIRECT_URL = "home"
-# Media configuration
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL='/media/'
